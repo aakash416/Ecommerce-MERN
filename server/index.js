1f1ece96bbf184b8725c6b5eafa04ecd247ee11b
Aakash Raj <aakashraj416@gmail.com>
2024-03-01 17:29:39 +0530

    bug error3

--- server/index.js
+++ server/index.js
@@ -19,7 +19,13 @@ connectDB();
 app.use(express.json())
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+
+const corsOptions = {
+    origin: 'https://ecommerce-mern-iota.vercel.app', // Replace this with the requesting origin or a function to dynamically set it
+    credentials: true, // To allow cookies and HTTP authentication
+};
+
+app.use(cors(corsOptions))
 
 app.get("/", (req, res) => {
     return res.status(200).json("Hi this is E-commerce Pojects")
