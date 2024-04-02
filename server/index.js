7496d7decaa269ca327d3a295bed3a7766f78506
Gosika <gosika.pooja@accenture.com>
2024-04-02 11:55:51 +0530

    login button disabled when user details are empty

--- server/index.js
+++ server/index.js
@@ -1,48 +1,45 @@
-import 'dotenv/config'
-import express from 'express'
-import cors from 'cors';
-import userRouter from './Routes/userRoutes.js'
-import sellerRouter from './Routes/sellerRoutes.js';
-import productRouter from './Routes/productRouter.js';
-import cartRouter from './Routes/cartRouter.js';
-import paymentRouter from './Routes/paymentRouter.js';
-import adminRouter from "./Routes/adminRouter.js"
-import cookieParser from 'cookie-parser';
-import connectDB from './Config/DB.js';
-import errorHandler from './Middleware/errorMiddleware.js'
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import userRouter from "./Routes/userRoutes.js";
+import sellerRouter from "./Routes/sellerRoutes.js";
+import productRouter from "./Routes/productRouter.js";
+import cartRouter from "./Routes/cartRouter.js";
+import paymentRouter from "./Routes/paymentRouter.js";
+import adminRouter from "./Routes/adminRouter.js";
+import cookieParser from "cookie-parser";
+import connectDB from "./Config/DB.js";
+import errorHandler from "./Middleware/errorMiddleware.js";
 
 const app = express();
-
 
 // Connect to Database
 connectDB();
 
-app.use(express.json())
+app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-    origin: process.env.ORIGIN,
-    credentials: true,
+  origin: process.env.ORIGIN,
+  credentials: true,
 };
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
-    return res.status(200).json("Hi this is E-commerce Pojects")
-})
+  return res.status(200).json("Hi this is E-commerce Pojects");
+});
 
-
-app.use('/api/users', userRouter)
-app.use('/api/sellers', sellerRouter)
-app.use('/api/product', productRouter)
-app.use('/api/cart', cartRouter)
-app.use('/api/payment', paymentRouter)
-app.use('/api/admin', adminRouter)
+app.use("/api/users", userRouter);
+app.use("/api/sellers", sellerRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/payment", paymentRouter);
+app.use("/api/admin", adminRouter);
 
 app.use(errorHandler);
 
-app.listen(process.env.PORT, () => { console.log(`Server is running on port ${process.env.PORT}`) })
-
-
-
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
